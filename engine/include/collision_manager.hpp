9661fae11260abe7666f2feb35c51272f637c2b7
Ateldy Brasil <ateldybfilho@gmail.com>
2017-08-27 20:24:37 -0300

    Application 3: Style Sheet

--- engine/include/collision_manager.hpp
+++ engine/include/collision_manager.hpp
@@ -5,31 +5,32 @@
 #include <list>
 #include <iterator>
 
-
 #include "sdl2core.hpp"
 #include "gameobject.hpp"
 
-namespace engine{
-  class CollisionManager{
-  public:
+namespace engine {
 
-    CollisionManager();
-    
-    ~CollisionManager();
-    
-    void getCollisions(std::list <GameObject*> *objects);
-    GameObject* checkCollision(GameObject* game_object,std::string type);
-  private:
+    class CollisionManager {
 
-    bool collide(GameObject* obj1, GameObject* obj2);
-    std::list <GameObject*>* collision_list;
+    public:
+        CollisionManager();
+        ~CollisionManager();
+      
+        void getCollisions( std::list <GameObject*> *objects );
 
-    SDL_Rect rect1;
-    SDL_Rect rect2;
-    SDL_Rect result;
+        GameObject* checkCollision( GameObject* game_object, std::string type );
 
+    private:
+        bool collide( GameObject* obj1, GameObject* obj2 );
 
+        std::list <GameObject*>* collision_list;
 
-  };
+        SDL_Rect rect1;
+        SDL_Rect rect2;
+        SDL_Rect result;
+
+    };
+
 }
-#endif
+
+#endif  // COLLISION_MANAGER_HPP_
