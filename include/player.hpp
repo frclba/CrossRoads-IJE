35c3473b24bc1a5fb51bdf43e23bfa727114a68f
Matias0422 <leguenta@hotmail.com>
2017-08-27 13:07:42 -0300

    Aplication 1 and 2

--- include/player.hpp
+++ include/player.hpp
@@ -1,5 +1,6 @@
 #ifndef __PLAYER_HPP__
 #define __PLAYER_HPP__
+
 #include <iostream>
 
 #include "components/component.hpp"
@@ -12,31 +13,33 @@
 
 using namespace engine;
 
-class Player : public Component{
+class Player : public Component {
+
 public:
-  Player(GameObject &main_game_object, std::string id, AnimationControllerComponent *animC, GameObject &attack_box,
-	 ImageComponent *background):
-    Component(main_game_object, id), side(false), attack_meele(false),attack_ranged(false), animCtrl(animC), stand(false), jump(false),
-    walkR(false), walkL(false), damageBool(false),  m_attack_box(&attack_box),
-    m_background(background){}
+
+    Player(GameObject &main_game_object, std::string id,
+         AnimationControllerComponent *animC, GameObject &attack_box,
+	       ImageComponent *background):
+         Component(main_game_object, id), side(false),
+         attack_meele(false), attack_ranged(false), animCtrl(animC),
+         stand(false), jump(false), walkR(false), walkL(false),
+         damageBool(false),  m_attack_box(&attack_box),
+         m_background(background){}
 
     ~Player();
 
     bool init();
-    void update();
+    bool has_ground();
 
+    void update();
     void is_dead();
     void damage_player();
     void attack_player();
     void move_player();
     void jump_player();
     void damage();
-
-
     void processPos();
     void gravityF();
-    bool has_ground();
-    //void monsterAI(GameObject *obj);
 
     GameObject *player;
     GameObject *plataform;
@@ -49,10 +52,13 @@ public:
     int life_points;
 
 private:
+
     const bool RIGHT = true;
     const bool LEFT = false;
+
     AnimationControllerComponent *animCtrl;
     AnimationControllerComponent *monster_controler;
+
     unsigned int jumptime;
     unsigned int time_attack;
 
@@ -64,9 +70,11 @@ private:
 
     GameObject *ground;
     GameObject* m_attack_box;
+
     float dy;
+
     ImageComponent* m_background;
 
 };
 
-#endif
+#endif // __PLAYER_HPP__
