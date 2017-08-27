902f0aec270b9159b3c16c752c3addfffb9c7c29
mendesiasmin <mendesiasmin96@gmail.com>
2017-08-27 16:03:11 -0300

    Fourth application: Style Sheet

--- engine/src/mouse.cpp
+++ engine/src/mouse.cpp
@@ -1,36 +1,41 @@
 #include "mouse.hpp"
 
-using namespace engine ;
+using namespace engine;
 
-void Mouse::set_position(){
-    int x,y;
+void Mouse::set_position() {
+
+    int x, y;
     SDL_GetMouseState(&x, &y);
     mouseX = x;
     mouseY = y;
+
 }
 
-bool Mouse::is_over(engine::GameObject *gameObject){
+bool Mouse::is_over(engine::GameObject *gameObject) {
 
-    if(
-            mouseX > gameObject->main_positionX &&
-            mouseY > gameObject->main_positionY &&
-            mouseX < (gameObject->main_width + gameObject->main_positionX) &&
-            mouseY < (gameObject->main_height + gameObject->main_positionY)
-    )
-    {
+    if( mouseX > gameObject->main_positionX &&
+        mouseY > gameObject->main_positionY &&
+        mouseX < (gameObject->main_width + gameObject->main_positionX) &&
+        mouseY < (gameObject->main_height + gameObject->main_positionY) ) {
+      return true;
+    }
+    return false;
+
+}
+
+bool Mouse::is_right_button() {
+
+    if( SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT) ) {
         return true;
     }
     return false;
 }
-bool Mouse::is_right_button(){
-    if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT)) {
+
+bool Mouse::is_left_button() {
+
+    if( SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_RIGHT) ) {
         return true;
     }
     return false;
-}
-bool Mouse::is_left_button(){
-    if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
-        return true;
-    }
-    return false;
+
 }
