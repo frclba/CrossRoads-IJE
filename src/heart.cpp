a580a0d277caa048a4d3099075ca810cf54fd002
mendesiasmin <mendesiasmin96@gmail.com>
2017-08-27 16:01:46 -0300

    Third application: Style Sheet

--- src/heart.cpp
+++ src/heart.cpp
@@ -1,17 +1,18 @@
-#include"heart.hpp"
+#include "heart.hpp"
 
-Heart::~Heart(){}
+Heart::~Heart() {}
 
-bool Heart::init(){
-  _main_game_object->setState(GameObject::State::enabled);
-  return true;
+bool Heart::init() {
+
+    _main_game_object->setState(GameObject::State::enabled);
+    return true;
+
 }
 
-void Heart::update(){
+void Heart::update() {
 
-  if(m_life > m_player->life_points){
-    _main_game_object->setState(GameObject::State::disabled);
-  }
-
+    if( m_life > m_player->life_points ) {
+        _main_game_object->setState(GameObject::State::disabled);
+    }
 
 }
