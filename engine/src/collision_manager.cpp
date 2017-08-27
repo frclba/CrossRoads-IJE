55e157ca6610a19fcc3d4958fd3adcedbd682f25
Ateldy Brasil <ateldybfilho@gmail.com>
2017-08-27 18:13:46 -0300

    Application 1: Style Sheet

--- engine/src/collision_manager.cpp
+++ engine/src/collision_manager.cpp
@@ -6,20 +6,28 @@ using namespace engine;
 CollisionManager::CollisionManager(){}
 CollisionManager::~CollisionManager(){}
 
-void CollisionManager::getCollisions(std::list <GameObject*> *objects){
-  collision_list = objects;
+void CollisionManager::getCollisions( std::list <GameObject*> *objects ) {
+    
+    collision_list = objects;
+
 }
 
-GameObject* CollisionManager::checkCollision(GameObject* game_object,std::string type){
-  for (std::list<GameObject*>::iterator obj = collision_list->begin(); obj != collision_list->end(); ++obj){
-    if((*obj)->type == type && collide(game_object,*obj)){
-      return (*obj);
+GameObject* CollisionManager::checkCollision( GameObject* game_object, 
+                                             std::string type ) {
+  
+    for (std::list<GameObject*>::iterator obj = collision_list->begin(); 
+         obj != collision_list->end(); ++obj){
+        if( ( *obj )->type == type && collide( game_object, *obj ) ) {
+            return (*obj);
+        }
     }
-  }
-  return NULL;
+
+    return NULL;
+
 }
 
-bool CollisionManager::collide(GameObject* obj1, GameObject* obj2){
+bool CollisionManager::collide( GameObject* obj1, GameObject* obj2 ) {
+  
   rect1.x = obj1->main_positionX; 
   rect1.y = obj1->main_positionY;
   rect1.h = obj1->main_height;
@@ -30,4 +38,5 @@ bool CollisionManager::collide(GameObject* obj1, GameObject* obj2){
   rect2.w = obj2->main_width;
 
   return SDL_IntersectRect(&rect1, &rect2, &result);
+
 }
