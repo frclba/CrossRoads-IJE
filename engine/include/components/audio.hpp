1538983398da582367e0530c6d124d2f2347c49a
Cauê <cauemateus@hotmail.com>
2017-08-28 14:24:00 -0300

    Application 2: Style Sheet

--- engine/include/components/audio.hpp
+++ engine/include/components/audio.hpp
@@ -1,5 +1,5 @@
-#ifndef __ENGINE_COMPONENTS_AUDIO__
-#define __ENGINE_COMPONENTS_AUDIO__
+#ifndef FOO_BAR_ENGINE_COMPONENTS_AUDIO_H_
+#define FOO_BAR_ENGINE_COMPONENTS_AUDIO_H_
 
 #include <iostream>
 
@@ -10,28 +10,37 @@
 namespace engine {
 
   class AudioComponent : public Component {
+
     public:
         enum class AudioState {
+
             playing,
             paused,
             stopped
+
         };
 
-        AudioComponent(GameObject &main_game_object, std::string id, std::string path, bool is_music=false,
-                       bool play_on_start=true)
-            : Component(main_game_object, id), m_path(path), m_is_music(is_music),
-              m_play_on_start(play_on_start), m_audio_state(AudioState::stopped),
-              m_music(NULL), m_sound(NULL) {}
+        AudioComponent( GameObject &main_game_object, std::string id, std::string path,
+                       bool is_music = false, bool play_on_start = true )
+            : Component( main_game_object, id ), m_path( path ), m_is_music( is_music ), 
+              m_play_on_start( play_on_start ), m_audio_state( AudioState::stopped ),
+              m_music( NULL ), m_sound( NULL ) {
+
+        }
 
         bool init();
         bool shutdown();
         void update();
 
-        void play(int loops=-1, int channel=-1);
-        void stop(int channel=-1);
-        void pause(int channel=-1);
+        void play( int loops = -1, int channel = -1 );
+        void stop( int channel = -1 );
+        void pause( int channel = -1 );
 
-        inline bool is_music() { return m_is_music; }
+        inline bool is_music() {
+
+            return m_is_music;
+
+        }
 
     private:
         std::string m_path;
@@ -41,6 +50,9 @@ namespace engine {
 
         Mix_Music * m_music;
         Mix_Chunk * m_sound;
+
     };
+
 }
-#endif
+
+#endif // FOO_BAR_ENGINE_COMPONENTS_AUDIO_H_
