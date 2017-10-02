1812f9a3c5414f1667d28f437b2908194f802c17
Carlos Aragon <carloseraragon@gmail.com>
2017-10-02 08:57:51 -0300

    Application 1 and 2 - tecnique 21

--- src/portal.cpp
+++ src/portal.cpp
@@ -18,51 +18,51 @@ bool Portal::init() {
         _main_game_object->main_positionY = 00;
     */
 
-    interator = 0;
-    timestep = 0;
-    monster_number = 0;
+    monster_number_iterator = 0;
+    time_between_monsters = 0;
+    monsters_out_of_portal = 0;
 
     return true;
 
 }
 
 /**
-    This method is responsible for updating portal positions in the game for 
+    This method is responsible for updating portal positions in the game for
     monsters to appear
 */
 
 void Portal::update() {
 
-    if( timestep < Game::instance.timer->getTicks() &&
-        m_monsters[interator]->state() == GameObject::State::disabled &&
-        _main_game_object->main_positionX > 0 && 
+    if( time_between_monsters < Game::instance.timer->getTicks() &&
+        m_monsters[monster_number_iterator]->state() == GameObject::State::disabled &&
+        _main_game_object->main_positionX > 0 &&
         _main_game_object->main_positionX + _main_game_object->main_width < 800
       ) {
 
-        m_monsters[interator]->main_positionX = _main_game_object->main_positionX;
-        m_monsters[interator]->main_positionY = _main_game_object->main_positionY;
-        m_monsters[interator]->setState(GameObject::State::enabled);
+        m_monsters[monster_number_iterator]->main_positionX = _main_game_object->main_positionX;
+        m_monsters[monster_number_iterator]->main_positionY = _main_game_object->main_positionY;
+        m_monsters[monster_number_iterator]->setState(GameObject::State::enabled);
 
-        monster_number++;
-        
-        timestep = Game::instance.timer->getTicks() + 3000;
+        monsters_out_of_portal++;
+
+        time_between_monsters = Game::instance.timer->getTicks() + 3000;
     }
 
-    interator = (interator + 1) % m_monsters.size();
+    monster_number_iterator = (monster_number_iterator + 1) % m_monsters.size();
 
-    if( monster_number <= 5 && m_background->imagePart->x == 0 ) {
+    if( monsters_out_of_portal <= 5 && m_background->imagePart->x == 0 ) {
         m_portal_pos->m_init_posX = 650;
     }
 
-    if( monster_number < 10 && monster_number > 5 ) {
+    if( monsters_out_of_portal < 10 && monsters_out_of_portal > 5 ) {
         m_portal_pos->m_init_posX = 1200;
     }
 
-    if( monster_number < 20 && monster_number > 10 ) {
+    if( monsters_out_of_portal < 20 && monsters_out_of_portal > 10 ) {
         m_portal_pos->m_init_posX = 1600;
     }
 
-    if( monster_number > 20 ) {
+    if( monsters_out_of_portal > 20 ) {
         _main_game_object->setState(GameObject::State::disabled);
     }
 
@@ -72,11 +72,11 @@ void Portal::update() {
 
 /**
     This method is responsible for add a monster no game through the portal
-    @param monster specifies the monster that enters the game 
+    @param monster specifies the monster that enters the game
 */
 void Portal::add_monster( GameObject* monster ){
-    
+
     monster->setState( GameObject::State::disabled );
     m_monsters.push_back( monster );
-    
+
 }
