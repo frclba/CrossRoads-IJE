d4a44b53cdb7470cca89b11f4f4b8e2cfbdfe4c0
Wallacy Braz <wallacy.braz@live.com>
2017-11-13 16:17:27 -0200

    Applications of technique: comment on control structures

--- src/bossAI.cpp
+++ src/bossAI.cpp
@@ -31,13 +31,14 @@ void Boss::update() {
 
     m_boss_animation->play_animation("boss_idle", true);
 
+    // if the boss component is on a valid horizontal position 
     if( _main_game_object->main_positionX > -10 &&
         _main_game_object->main_positionX +
         _main_game_object->main_width < 850 ) {
-
+        // if boss is performing a dash attack, updates its position.
         if( is_dash_attacking ) {
             boss_move();
-        }
+        } 
         else {
 
             // Do nothing
@@ -45,23 +46,23 @@ void Boss::update() {
         }
 
         m_boss_animation->flipping(!is_in_corner);
-
+        // if it is time for updating boss component
         if( boss_update_time < Game::instance.timer->getTicks() ) {
 
             if( m_player->main_positionY > 300 ) {
                 is_dash_attacking = true;
                 is_fireball_attacking = false;
                 m_fireball->setState(GameObject::State::disabled);
-            }
+            } // if 
             else {
                 is_dash_attacking = false;
                 is_fireball_attacking = true;
                 m_boss_animation->play_animation("boss_howl");
                 m_fireball->setState(GameObject::State::enabled);
-            }
+            } // else
 
             boss_update_time = Game::instance.timer->getTicks() + 3000;
-        }
+        } // if
         else {
 
             // Do nothing
@@ -89,12 +90,14 @@ void Boss::boss_damage() {
     AudioComponent * boss_in_rage_audio = (dynamic_cast<AudioComponent*> (
                                                _main_game_object->get_component(
                                                "boss_in_rage_audio")));
-
+  
+    // if the boss is being attacked , reduces boss life points.
     if( Game::instance.collision_manager->checkCollision(
         _main_game_object, "attack_box") ||
         Game::instance.collision_manager->checkCollision(
         _main_game_object, "bullet") ) {
-
+        
+        // checks and updates the time that boss calcutes its damage
         if( damage_time < Game::instance.timer->getTicks() ) {
             boss_life--;
             damage_time = Game::instance.timer->getTicks() + 1000;
@@ -144,7 +147,8 @@ void Boss::boss_move() {
     boss_dash_audio->play(0, -1);
 
     m_boss_animation->play_animation("boss_dash", true);
-
+    
+    // checks if it is time for updating the delay between the boss component movement.
     if( boss_movement_time_gap < Game::instance.timer->getTicks() ) {
         // is_in_corner = !is_in_corner;
         boss_movement_time_gap = Game::instance.timer->getTicks() + 900;
